84f20b0aaffc3959260530cd9b456d5b477c1637
Luis Miguel <melovasquezl@gmail.com>
2020-10-29 20:26:13 -0400

    build

---
+++ build/precache-manifest.99f5ab339fc64c350e8af8ce3deb6726.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93c2e439745f8b5429e7ead87c9bfcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96661d62d0e2486331ae",
+    "url": "/static/css/main.00fd3dd2.chunk.css"
+  },
+  {
+    "revision": "6398ca11b4cedf78d13f",
+    "url": "/static/js/2.bbd66ed5.chunk.js"
+  },
+  {
+    "revision": "96661d62d0e2486331ae",
+    "url": "/static/js/main.6abb3b92.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6dbba458959d4ce1edd2f5b3ab3ae13b",
+    "url": "/static/media/logo.6dbba458.svg"
+  },
+  {
+    "revision": "cdaf5beb3d7430d08130e83b9f95cbd1",
+    "url": "/static/media/profile.cdaf5beb.png"
+  }
+]);
